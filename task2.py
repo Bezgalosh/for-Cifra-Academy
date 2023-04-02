8ca6af9ee4d12a503102cd47ebb12ade93b0d50c
Bezgalosh <106178413+Bezgalosh@users.noreply.github.com>
2023-04-02 16:31:31 +0400

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,19 @@
+import argparse
+import math
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", help="filename with numbers") 
+args = parser.parse_args()
+
+with open(args.filename, 'r') as f:
+    nums = [int(line.strip()) for line in f]
+
+nums.sort()
+if len(nums) % 2 == 0:
+    median = (nums[len(nums)//2 - 1] + nums[len(nums)//2]) / 2
+else:
+    median = nums[len(nums)//2]
+
+moves = int(sum(abs(num - median) for num in nums))
+
+print(moves)
